32f8d38c0b00c74b7dfd3c3080d9e1000fee8405
pduran17 <110075714+pduran17@users.noreply.github.com>
2022-07-28 17:18:31 -0600

    Update COMPARATOR.v

--- COMPARATOR.v
+++ COMPARATOR.v
@@ -8,12 +8,14 @@ Autor: Pablo Duran Segura */
 
 module  COMPARATOR( input A,B,   //entradas, cada una de 1 bit
                  output K,L);  //salidas, cada una de 1 bit
+reg [3:0] I_K = 4'b1011;
+reg [3:0] I_L = 4'b1101;
 
-reg [3:0]  I;
+reg [1,0] S = {A,B};
 
 //instancio el modulo MUX para definir las entradas, lineas de seleccion y salidas diseñadas con los mapas de Karnaugh y las tablas de verdad
-MUX4x1_Case MuxK (.I({1,0,1,1}), .S({A,B}), Y.(K)); //multiplexor que resuelve K
-MUX4x1_Case MuxL (.I({1,1,0,1}), .S({A,B}), Y.(K));  //multiplexor que resuelve L
+MUX4x1_Case MuxK (.I(I_K), .S(S), Y.(K)); //multiplexor que resuelve K
+MUX4x1_Case MuxL (.I(I_L), .S(S), Y.(K));  //multiplexor que resuelve L
   
 
 endmodule   
